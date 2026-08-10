51cc6834655eaf7b0984a057ff2c7145dd7ede67
Yashwanth D <yashwanthdevendran19@gmail.com>
2026-08-10 07:36:57 +0530

    Time: 3 ms (99.74%) | Memory: 42.6 MB (68.41%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+
+        
+            if (!dp[i]) {
+
+                for (int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+
+               
+                if (dp[n]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
